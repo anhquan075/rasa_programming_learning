514e0086a48ef1177a5c66af838a4bc65a373100
anhquan075 <19522081@gm.uit.edu.vn>
2022-02-27 12:53:11 +0000

    Test travis CI

---
+++ tests/test_nlu.py
@@ -0,0 +1,15 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+
+
+def test_nlu_interpreter():
+    training_data = load_data("data/nlu_converted.md")
+    trainer = Trainer(config.load("config.yml"))
+    interpreter = trainer.train(training_data)
+    test_interpreter_dir = trainer.persist(
+        "./tests/models", project_name="nlu")
+    parsing = interpreter.parse('xin chào')
+
+    assert parsing['intent']['name'] == 'greeting'
+    assert test_interpreter_dir
