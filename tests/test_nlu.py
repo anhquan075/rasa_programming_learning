4bf32b3bb30dcf183238e4f755e90f341fac73e0
anhquan075 <19522081@gm.uit.edu.vn>
2022-02-27 15:23:53 +0000

    Update README.md

--- tests/test_nlu.py
+++ tests/test_nlu.py
@@ -4,12 +4,15 @@ from rasa_nlu import config
 
 
 def test_nlu_interpreter():
-    training_data = load_data("../data/nlu_converted.yml")
+    training_data = load_data("data/nlu_converted.yml")
     trainer = Trainer(config.load("../config.yml"))
     interpreter = trainer.train(training_data)
     test_interpreter_dir = trainer.persist(
         "./tests/models", project_name="nlu")
     parsing = interpreter.parse('xin chào')
+    print(parsing)
 
     assert parsing['intent']['name'] == 'greeting'
     assert test_interpreter_dir
+
+test_nlu_interpreter()
